8de0f7defc92d0834139924c011c94463a8ca8e8
Angelou Sereño <angeloubendebelsereno@gmail.com>
2023-05-22 01:16:40 +0800

    initializeFXGL
    initializesFXGL as scaffold

---
+++ BraveDefenders/src/main/java/game/App.java
@@ -0,0 +1,20 @@
+package game;
+
+import com.almasb.fxgl.app.GameApplication;
+import com.almasb.fxgl.app.GameSettings;
+
+public class App extends GameApplication {
+
+	@Override
+	protected void initSettings(GameSettings settings) {
+		// TODO Auto-generated method stub
+		settings.setTitle("Brave Defenders");
+
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		launch(args);
+	}
+
+}
